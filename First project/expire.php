7a071ca51b0392ec9ab3e22495cfa45b15423cb4
Fenrir Eru <139980729+Ramanilovatar@users.noreply.github.com>
2025-04-23 17:15:52 +0330

    Delete First project directory

--- First project/expire.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-setcookie('user_remember', $_SESSION['username'], time() - 1, '/');
-setcookie('pass_remember', $_SESSION['password'], time() - 1, '/');
-session_destroy();
-header("location:index.php");
